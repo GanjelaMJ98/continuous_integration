7dc4ede58e7015ffef8f89839a8496619be75617
GanjelaMJ98 <ganjela98@gmail.com>
2021-04-11 13:55:41 +0300

    First commit
    Signed-off-by: GanjelaMJ98 <ganjela98@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "lib.h"
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Version " << version() << " was started" << std::endl;
+    std::cout << "Hello, World! " << std::endl;
+    return 0;
+}
